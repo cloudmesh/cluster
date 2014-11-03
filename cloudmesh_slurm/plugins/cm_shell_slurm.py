f1fc859f522564199aef05da44fe34d4f192f15f
Gregor von Laszewski <laszewski@gmail.com>
2014-11-03 13:38:22 -0500

    fixing merge conflicts

--- cloudmesh_slurm/plugins/cm_shell_slurm.py
+++ cloudmesh_slurm/plugins/cm_shell_slurm.py
@@ -1,16 +1,15 @@
-<<<<<<< HEAD:cloudmesh_cluster/plugins/cm_shell_cluster.py
 from cmd3.shell import command
 from cloudmesh_common.logger import LOGGER
 import cloudmesh
-from cloudmesh_cluster.api.virtual_cluster import virtual_cluster
+from cloudmesh_slurm.api.virtual_slurm_cluster import virtual_slurm_cluster
 
 log = LOGGER(__file__)
 
 
-class cm_shell_cluster:
-    """Creating a virtual cluster"""
+class cm_shell_slurm:
+    """Creating a virtual slurm cluster"""
 
-    def activate_cm_shell_cluster(self):
+    def activate_cm_shell_slurm(self):
         self.register_command_topic('cloud', 'slurm')
         pass
 
@@ -28,13 +27,13 @@ class cm_shell_cluster:
             slurm list
             slurm default NAME WORKERS CLOUD [--image=IMAGE] [--flavor=FLAVOR]
 
-        Manages a virtual cluster on a cloud
+        Manages a virtual slurm cluster on a cloud
 
         Arguments:
 
-          NAME     The name of the cluster
-          WORKERS  The number of workers in the virtual cluster
-          CLOUD    The name of the cloud on which the virtual cluster
+          NAME     The name of the slurm cluster
+          WORKERS  The number of workers in the virtual slurm cluster
+          CLOUD    The name of the cloud on which the virtual slurm cluster
                    is to be deployed
 
         Options:
@@ -48,125 +47,52 @@ class cm_shell_cluster:
             log.info("clean the vm")
             return
 
-        if arguments["default"] and arguments["NAME"] and arguments["WORKERS"] and arguments["CLOUD"]:
-           virtual_cluster().defaults("{NAME}".format(**arguments),"{WORKERS}".format(**arguments),"{CLOUD}".format(**arguments),"{--image}".format(**arguments),"{--flavor}".format(**arguments))
+        if (arguments["default"] and
+            arguments["NAME"] and
+            arguments["WORKERS"] and
+            arguments["CLOUD"]):
+           virtual_slurm_cluster().defaults("{NAME}".format(**arguments),
+                                      "{WORKERS}".format(**arguments),
+                                      "{CLOUD}".format(**arguments),
+                                      "{--image}".format(**arguments),
+                                      "{--flavor}".format(**arguments))
            return
-
-        if arguments["create"] and arguments["NAME"] and arguments["WORKERS"] and arguments["CLOUD"]:
-           virtual_cluster().create("{NAME}".format(**arguments),"{WORKERS}".format(**arguments),"{CLOUD}".format(**arguments),"{--image}".format(**arguments),"{--flavor}".format(**arguments))
-           return
-        elif arguments["create"]:
-           virtual_cluster().create_defaults()
-           return
-
-        if arguments["delete"] and arguments["NAME"]:
-            virtual_cluster().delete("{NAME}".format(**arguments))
-            log.info("delete the cluster '{NAME}'".format(**arguments))
-            return
-        elif arguments["delete"]:
-            virtual_cluster().delete_all()
-            log.info("delete all the clusters")
-            return
-
-        if arguments["info"] and arguments["NAME"]:
-            virtual_cluster().info("{NAME}".format(**arguments))
-            log.info("info of cluster {NAME}".format(**arguments))
-            return
-        elif arguments["info"]:
-            virtual_cluster().info_all()
-            log.info("info of all clusters")
-            return
-
-        if arguments["list"]:
-            virtual_cluster().list_clusters()
-            log.info("list of all clusters")
-            return
-
-        return
-=======
-from cmd3.shell import command
-from cloudmesh_common.logger import LOGGER
-import cloudmesh
-from cloudmesh_slurm.api.virtual_slurm import virtual_slurm
-
-log = LOGGER(__file__)
-
-class cm_shell_slurm:
-    """Creating a virtual cluster"""
-
-    def activate_cm_shell_slurm(self):
-        self.register_command_topic('cloud', 'cluster')
-        pass
-
-    @command
-    def do_slurm(self, args, arguments):
-        """
-        Usage:
-            cluster create NAME WORKERS CLOUD [--image=IMAGE] [--flavor=FLAVOR]
-            cluster info [NAME]
-            cluster status [NAME]            
-            cluster delete [-f] [NAME]
-            cluster clean
-            cluster checkpoint NAME
-            cluster restore NAME
-            cluster list
-
-        Manages a virttual cluster on a cloud
-
-        Arguments:
-          NAME     The name of the cluster
-          WORKERS  The number of workers in the virtual cluster
-          CLOUD    The name of the cloud on which the virtual cluster
-                   is to be deployed
-
-        Options:
-           -v       verbose mode
-
-        """
-        log.info(arguments)
-
-        if arguments["clean"]:
-            log.info("clean the vm")
-            return
 
         if (arguments["create"] and
             arguments["NAME"] and
             arguments["WORKERS"] and
             arguments["CLOUD"]):
-           virtual_slurm().create("{NAME}".format(**arguments),
+           virtual_slurm_cluster().create("{NAME}".format(**arguments),
                                     "{WORKERS}".format(**arguments),
                                     "{CLOUD}".format(**arguments),
                                     "{--image}".format(**arguments),
                                     "{--flavor}".format(**arguments))
-
+           return
+        elif arguments["create"]:
+           virtual_slurm_cluster().create_defaults()
+           return
 
         if arguments["delete"] and arguments["NAME"]:
-            virtual_slurm().delete("{NAME}".format(**arguments))
-            log.info("delete the cluster '{NAME}'".format(**arguments))
+            virtual_slurm_cluster().delete("{NAME}".format(**arguments))
+            log.info("delete the slurm cluster '{NAME}'".format(**arguments))
             return
-
         elif arguments["delete"]:
-            print arguments
-            virtual_slurm().delete_all()
-            log.info("delete all the clusters")
+            virtual_slurm_cluster().delete_all()
+            log.info("delete all the slurm clusters")
             return
 
         if arguments["info"] and arguments["NAME"]:
-            virtual_slurm().info("{NAME}".format(**arguments))
-            log.info("info of cluster {NAME}".format(**arguments))
+            virtual_slurm_cluster().info("{NAME}".format(**arguments))
+            log.info("info of slurm cluster {NAME}".format(**arguments))
             return
-
         elif arguments["info"]:
-            virtual_slurm().info_all()
-            log.info("info of all clusters")
+            virtual_slurm_cluster().info_all()
+            log.info("info of all slurm clusters")
             return
 
-        if arguments["list"] and arguments["NAME"]:
-            log.info("list of cluster {NAME}".format(**arguments))
-            return
-
-        elif arguments["list"]:
-            log.info("list of all clusters")
+        if arguments["list"]:
+            virtual_slurm_cluster().list_clusters()
+            log.info("list of all slurm clusters")
             return
 
         return
