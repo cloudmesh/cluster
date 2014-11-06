35d775c612e0cc872e24a98970a0e08ec918dea8
Satwik Narlanka <satwik.602@gmail.com>
2014-11-06 16:55:37 -0500

    created a hadoop class

---
+++ cloudmesh_slurm/api/virtual_hadoop.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import cloudmesh
+from cloudmesh_common.logger import LOGGER
+from subprocess import call
+
+log= LOGGER(__file__)
+
+class virtual_hadoop:
+    def create(self,name,datanodes):
+        print ("create hadoop cluster {0}"
+               "with {1} datanodes".format(name,datanodes))
+        return
+
+    return
