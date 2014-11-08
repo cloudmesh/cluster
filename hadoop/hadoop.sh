03875e437e279b4a500e99a72788e257695adaca
Satwik Narlanka <satwik.602@gmail.com>
2014-11-07 23:13:29 -0500

    removed syntax error

--- hadoop/hadoop.sh
+++ hadoop/hadoop.sh
@@ -2,7 +2,7 @@ echo -e "Y" | sudo apt-get install pdsh
 pdsh -R ssh -w $1,$2 sudo apt-get update
 pdsh -R ssh -w $1,$2 echo -e "Y" | sudo apt-get install default-jdk
 pdsh -R ssh -w $1,$2 wget http://www.us.apache.org/dist/hadoop/common/hadoop-2.5.0/hadoop-2.5.0.tar.gz
-pdsh -R ssh -w $1,$$2 tar -xzf hadoop-2.5.0.tar.gz
+pdsh -R ssh -w $1,$2 tar -xzf hadoop-2.5.0.tar.gz
 pdsh -R ssh -w $1,$2 mv hadoop-2.5.0 hadoop
 pdsh -R ssh -w $1,$2 echo "export HADOOP_HOME=~/hadoop" >> .bashrc
 pdsh -R ssh -w $1,$2 echo "export JAVA_HOME=/usr" >> .bashrc
